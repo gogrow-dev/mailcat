b4a0ee4b5f0f58fcf77f247471b5bdbbb5bdab97
Micaela Lorenzo <micaela@gogrow.dev>
2025-04-10 10:09:56 -0300

    fix default assignment to use env variable

--- lib/mailcat.rb
+++ lib/mailcat.rb
@@ -10,6 +10,14 @@ module Mailcat
 
   include ::ActiveSupport::Configurable
 
-  config_accessor :mailcat_api_key, default: ENV["MAILCAT_API_KEY"]
-  config_accessor :mailcat_url, default: ENV["MAILCAT_URL"]
+  config_accessor :mailcat_api_key, default: -> { ENV["MAILCAT_API_KEY"] }
+  config_accessor :mailcat_url, default: -> { ENV["MAILCAT_URL"] }
+
+  def self.mailcat_api_key_raw
+    config.mailcat_api_key.is_a?(Proc) ? config.mailcat_api_key.call : config.mailcat_api_key
+  end
+
+  def self.mailcat_url_raw
+    config.mailcat_url.is_a?(Proc) ? config.mailcat_url.call : config.mailcat_url
+  end
 end
